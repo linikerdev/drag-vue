892da55952fdd5d9737d26cbd138e705c9f63427
Liniker <contato@liniker.com.br>
2019-04-04 23:54:30 -0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div
+    class="box"
+    @drop.capture="isActiveDrag($event)"
+    @dragover.prevent.stop
+    @dragleave="hiddenDrag($event)"
+  >
+    <div class="divx" @dragover.prevent="showDrag($event)" id="list" v-if="!statusDiv"></div>
+    <div class="form" v-if="statusDiv">
+      <input type="file" class="filex" multiple>
+    </div>
+    {{statusDiv}}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      valorx: null,
+      statusDiv: false
+    };
+  },
+  methods: {
+    isActiveDrag(e) {
+      if (e.target.className !== "filex") {
+        e.preventDefault();
+      }
+    },
+    showDrag(e) {
+      this.statusDiv = true;
+    },
+    hiddenDrag(e) {
+      if (e.target.className === "filex") {
+        this.statusDiv = false;
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.box {
+  background: #eee;
+  width: 100%;
+  height: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .mostraDrag {
+    .hide {
+      display: none;
+    }
+  }
+  .form {
+    background-color: #ccc;
+    width: 300px;
+    height: 300px;
+    position: relative;
+
+    .filex {
+      background: red;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      width: 100%;
+    }
+  }
+  .divx {
+    background: #ccc;
+    width: 300px;
+    height: 300px;
   }
 }
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
+
